193bde2838a908813e234ab59dd39f06f0766f85
Marissa Fortier <marissafortier@gmail.com>
2020-02-23 21:50:40 -0500

    Updates

---
+++ precache-manifest.f21ad654f2dabe88b994c2e1d17c89c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cee3aa3adf7d08ed547d920bbac9559",
+    "url": "/%3Creact-portfolio%3E/index.html"
+  },
+  {
+    "revision": "651ea255bfbd133f0b91",
+    "url": "/%3Creact-portfolio%3E/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "55507184adb2556b4773",
+    "url": "/%3Creact-portfolio%3E/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "651ea255bfbd133f0b91",
+    "url": "/%3Creact-portfolio%3E/static/js/2.0b234f69.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/%3Creact-portfolio%3E/static/js/2.0b234f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55507184adb2556b4773",
+    "url": "/%3Creact-portfolio%3E/static/js/main.cd83b58a.chunk.js"
+  },
+  {
+    "revision": "eaa0ced7fefdd0a76b92",
+    "url": "/%3Creact-portfolio%3E/static/js/runtime-main.b5221b24.js"
+  }
+]);
